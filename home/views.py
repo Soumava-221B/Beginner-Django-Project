e86d5fa142516e15bc6d9f3786374ef7a74fe8d0
Soumava Das <soumavadas06@gmail.com>
2025-03-22 18:02:09 +0530

    Module 2 completed authentication

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 import pytz
 
 def home(request):
@@ -8,3 +9,7 @@ def home(request):
     now_ist = datetime.now(ist).strftime("%I:%M %p, %A, %d %B %Y")  # 12-hour format, day, date
 
     return render(request, 'home/welcome.html', {'today': now_ist})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
