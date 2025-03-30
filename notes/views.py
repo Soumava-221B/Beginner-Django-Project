de3859d9ffcbddbc2e6757db262e0611f148d094
Soumava Das <soumavadas06@gmail.com>
2025-03-30 22:14:49 +0530

    admin authentication and user model added

--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,7 @@ from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -26,11 +27,21 @@ class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
     
 class PopularNotesListView(ListView):
     model = Notes
